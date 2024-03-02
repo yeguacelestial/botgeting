40e65a0cf2a4ea74aa757a2d6e9b83713c73ff1c
Carlos Nava <yeguacelestial@gmail.com>
2024-03-02 17:17:51 -0600

    Separate commands from bot handler

--- bot.py
+++ bot.py
@@ -2,28 +2,20 @@ import logging
 
 from settings import TOKEN
 
-from telegram import Update
 from telegram.ext import (
     CommandHandler,
-    ContextTypes,
     ApplicationBuilder,
 )
+
+from commands import start
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
+application = ApplicationBuilder().token(TOKEN).build()
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm botgeting!"
-    )
+start_handler = CommandHandler("start", start)
+application.add_handler(start_handler)
 
-
-if __name__ == "__main__":
-    application = ApplicationBuilder().token(TOKEN).build()
-
-    start_handler = CommandHandler("start", start)
-    application.add_handler(start_handler)
-
-    application.run_polling()
+application.run_polling()
---
+++ commands.py
@@ -0,0 +1,9 @@
+from telegram import Update
+
+from telegram.ext import ContextTypes
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm botgeting!"
+    )
